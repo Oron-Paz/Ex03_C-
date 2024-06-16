3994ed61f973431ec2c3b5821db0344ad5dceea9
Oron Paz <oron.paz26@gmail.com>
2024-06-16 16:50:55 +0300

    fixes

--- Ex03.GarageLogic/VehicleFactory.cs
+++ Ex03.GarageLogic/VehicleFactory.cs
@@ -13,13 +13,13 @@ namespace Ex03.GarageLogic
         // Method to create a vehicle based on the specified type
 
         // Private methods to create specific types of vehicles
-        public static FuelCar CreateFuelCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, VehicleStatus i_vehicleStatus, CarColor i_color, int i_doors, float i_remainingFuelLiters)
+        public static FuelCar CreateFuelCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, VehicleStatus i_vehicleStatus, CarColor i_color, int i_doors)
         {  
             float i_remainingFuelLiters = i_remainingEnergy/100 * 45f;
             return new FuelCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, VehicleStatus.InRepair, i_color, i_doors, i_remainingFuelLiters);
         }
 
-        public static ElectricCar CreateElectricCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, VehicleStatus i_vehicleStatus, CarColor i_color, int i_doors, float i_remainingEngineTime)
+        public static ElectricCar CreateElectricCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, VehicleStatus i_vehicleStatus, CarColor i_color, int i_doors)
         {
             float i_remainingEngineTime = i_remainingEnergy/100 * 3.5f;
             return new ElectricCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, VehicleStatus.InRepair, i_color, i_doors, i_remainingEngineTime);
