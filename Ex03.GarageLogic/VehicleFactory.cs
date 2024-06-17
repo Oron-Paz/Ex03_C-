6a5260eebbffd929931646ee8cb0fbd157d5809e
Oron Paz <oron.paz26@gmail.com>
2024-06-17 09:07:32 +0300

    fixes

--- Ex03.GarageLogic/VehicleFactory.cs
+++ Ex03.GarageLogic/VehicleFactory.cs
@@ -15,14 +15,16 @@ namespace Ex03.GarageLogic
         // Private methods to create specific types of vehicles
         public static FuelCar CreateFuelCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, eVehicleStatus i_vehicleStatus, eCarColor i_color, int i_doors, float i_remainingFuelLiters)
         {  
-            
-            return new FuelCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, eVehicleStatus.InRepair, i_color, i_doors, i_remainingFuelLiters);
+            float remainingFuelLiters = i_remainingEnergy/100 * 45;
+
+            return new FuelCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, eVehicleStatus.InRepair, i_color, i_doors, remainingFuelLiters);
         }
 
         public static ElectricCar CreateElectricCar(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, eVehicleStatus i_vehicleStatus, eCarColor i_color, int i_doors, float i_remainingEngineTime)
         {
-            
-            return new ElectricCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, eVehicleStatus.InRepair, i_color, i_doors, i_remainingEngineTime);
+            float remainingEngineTime = i_remainingEnergy/100 * 3.5f;
+
+            return new ElectricCar(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, eVehicleStatus.InRepair, i_color, i_doors, remainingEngineTime);
         }
 
         public static FuelMotorcycle CreateFuelMotorcycle(string i_modelName, string i_licenseNumber, float i_remainingEnergy, string i_OwnerName, string i_OwnerPhoneNumber, eVehicleStatus i_vehicleStatus, eLicenseType i_licenseType, int i_engineVolume, float i_remainingFuelLiters)
@@ -37,6 +39,7 @@ namespace Ex03.GarageLogic
         {
             
             float remainingEngineTime = i_remainingEnergy/100 * 2.5f;
+
             return new ElectricMotorcycle(i_modelName, i_licenseNumber, i_remainingEnergy, i_OwnerName, i_OwnerPhoneNumber, i_vehicleStatus, i_licenseType, i_engineVolume, remainingEngineTime);
         }
 
